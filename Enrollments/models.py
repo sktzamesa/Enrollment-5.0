7f473ffeddf4b75b01691b42e87c68ca821975fc
John Phillippe <zjohnphillippe@gmail.com>
2024-12-04 21:40:43 +0800

    first commit

--- Enrollments/models.py
+++ Enrollments/models.py
@@ -5,26 +5,26 @@ from django.conf import settings
 
 class Admission(models.Model):
     class Applicant(models.TextChoices):
-        Senior_High_School_Graduate = 'SH','Senior HighSchool Graduate'
-        Alternative_Learning_System = 'AL', 'Alternative Learning System'
-        Currently_enrolled_Grade12_student= 'CE','Currently enrolled Grade12 student'
-        Bachelor_degree_graduate = 'BD','Bachelor degree graduate'
-        Teacher_certificate_program_applicant = 'TC','Teacher certificate program applicant'
-        Transferee = 'TF','Transferee'
-        Diploma_certificate_associate_vocationalgraduate = 'DC','Diploma certificate associate vocational graduate'
+        Senior_High_School_Graduate = 'Senior HighSchool Graduate','Senior HighSchool Graduate'
+        Alternative_Learning_System = 'Alternative Learning System', 'Alternative Learning System'
+        Currently_enrolled_Grade12_student= 'Currently enrolled Grade12 student','Currently enrolled Grade12 student'
+        Bachelor_degree_graduate = 'Bachelor degree graduate','Bachelor degree graduate'
+        Teacher_certificate_program_applicant = 'Teacher certificate program applicant','Teacher certificate program applicant'
+        Transferee = 'Transferee','Transferee'
+        Diploma_certificate_associate_vocationalgraduate = 'Diploma certificate associate vocational graduate','Diploma certificate associate vocational graduate'
     
     class SHSTrack(models.TextChoices):
-        academic = 'AC','Academic'
-        arts_and_design = 'AD','Arts and Design'
-        sports = 'SP','Sports'
-        TVL = 'TL','Technical Vocational Livelihood'
+        academic = 'Academic','Academic'
+        arts_and_design = 'Arts and Design','Arts and Design'
+        sports = 'Sports','Sports'
+        TVL = 'Vocational','Technical Vocational Livelihood'
 
     class SHSStrand(models.TextChoices):
-        stem = 'ST','Science, Technology, Engineering and Mathematics'
-        ict = 'it','Information Technology'
+        stem = 'Science, Technology, Engineering and Mathematics','Science, Technology, Engineering and Mathematics'
+        ict = 'Information Technology','Information Technology'
 
     class PreferredProgram(models.TextChoices):
-        BSCS = 'BC','BS Computer Science'
+        BSCS = 'BS Computer Science','BS Computer Science'
         BSIT = 'BT','BS Information Technology'
 
     class IncomeBracket(models.TextChoices):
@@ -33,28 +33,28 @@ class Admission(models.Model):
         HIGH = '2', '50k-100k'
         VERY_HIGH = '3', '100k+'
     class Waitlist(models.TextChoices):
-        PENDING = 'PE', 'PENDING'
-        DONE = 'DO', 'DONE'
+        PENDING = 'PENDING', 'PENDING'
+        DONE = 'DONE', 'DONE'
 
 
     Type_of_applicant = models.CharField(
-        max_length= 2,
+        max_length= 49,
         choices=Applicant,
     )
     senior_high_school_track = models.CharField(
-        max_length= 2,
+        max_length= 15,
         choices = SHSTrack,
     )
     Senior_high_school_strand = models.CharField(
-        max_length= 2,
+        max_length= 48,
         choices=SHSStrand,
     )
     Preferred_program = models.CharField(
-        max_length= 2,
+        max_length= 19,
         choices = PreferredProgram,
     )
     Waitlist = models.CharField(
-        max_length=2,
+        max_length=7,
         choices=Waitlist,
         default = Waitlist.PENDING
     )
@@ -119,6 +119,7 @@ class Admission(models.Model):
         upload_to='Admission/passport/%Y/%m/%d',
         blank=True
     )
+    Schedule_appointment = models.DateField()
     create_at = models.DateTimeField(auto_now_add=True)
     publish_at = models.DateTimeField(default=timezone.now)
     class Meta:
